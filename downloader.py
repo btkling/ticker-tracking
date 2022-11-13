3f39ea80cd0ae1e7d0ca72c6883e13ff757e0164
Ben Kling <kling.ben@gmail.com>
2022-11-13 17:39:18 -0500

    finish downloader module

--- downloader.py
+++ downloader.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import yfinance as yf
+import datetime as dt
 
 def load_symbols(fp:str) -> list():
     '''Loads symbols to track'''
@@ -8,4 +9,23 @@ def load_symbols(fp:str) -> list():
 
     return symbols
 
-def download_tickerdata() -> pd.DataFrame:
+def download_tickerdata(symbols:list(), ndays:int) -> pd.DataFrame:
+    '''given a list of symbols and a time period, retrieve the
+    latest stock information.'''
+
+    end_date = dt.date.today()
+    start_date = end_date - dt.timedelta(days = ndays)
+
+    end_date_str = end_date.isoformat()
+    start_date_str = start_date.isoformat()
+
+    tickerdata = yf.download(
+        tickers = symbols,
+        group_by = "Ticker",
+        start = start_date_str,
+        end = end_date_str
+    )
+
+    tickerdata = tickerdata.stack(level=0).rename_axis(["Date","Ticker"]).reset_index(level=1)
+
+    return tickerdata
