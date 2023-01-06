6c20a7a3839798e92f456731dfe1e2bf864c2866
Ben Kling <kling.ben@gmail.com>
2023-01-06 09:28:22 -0500

    branded variable names

--- statementparser.py
+++ statementparser.py
@@ -3,30 +3,30 @@ import re
 
 
 def main():
-    file_path = '/mnt/e/git/ticker-tracking/data/schwab/'
+    base_fp = '/mnt/e/git/ticker-tracking/data/'
+    schwabpath = f'{base_fp}schwab/'
     file_name = 'XXXXX911_Transactions_20230104-181416.csv'
     start_date = "2022-11-09"
     end_date = "2023-01-01"
-    schwabdf = read_schwab_statement(file_path, file_name)
+    schwabdf = read_schwab_statement(schwabpath, file_name)
 
     schwabdf = clean_dates(schwabdf, "Date")
 
-    valid_actions = [
+    schwab_actions = [
         'Buy',
         'Sell',
         'Reinvest Shares',
     ]
-    schwabdf = filter_schwab_actions(schwabdf, valid_actions)
+    schwabdf = filter_schwab_actions(schwabdf, schwab_actions)
 
-    numbercols = [
+    schwab_numbers = [
         "Quantity", 
         "Price", 
         "Fees & Comm", 
         "Amount"
     ]
-    for col in numbercols:
+    for col in schwab_numbers:
         schwabdf = clean_numbercol(schwabdf, col)
-
 
     schwab_symbols = [
         'LGILX',
@@ -38,9 +38,11 @@ def main():
     schwabdf = filter_symbols(schwabdf, "Symbol", schwab_symbols)
 
     schwabdf = filter_date(schwabdf, "Date", start_date, end_date)
-    print(schwabdf.info())
-    print(schwabdf.head())
+
     schwabdf.to_csv('/mnt/e/git/ticker-tracking/data/clean_statements/schwab.csv', index=False)
+
+
+
 
 
 def read_schwab_statement(file_path, file_name):
@@ -50,6 +52,15 @@ def read_schwab_statement(file_path, file_name):
         skiprows=1
     )
     return schwabdf
+
+
+def read_merrill_statement(file_path, file_name):
+    full_path = f"{file_path}{file_name}"
+    mdf = pd.read_csv(
+        full_path,
+        skiprows=1
+    )
+    return mdf
 
 
 def clean_dates(df, datecol):
